10ff1c6c841def721238e12a84b975b6cb02a4f8
ErgothDK <ergothdk@gmail.com>
2023-12-13 16:33:00 -0600

    Created separated component for TodoItem

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Todo from '../models/todo';
+import TodoItem from './TodoItem';
 
 const Todos: React.FC<{
     items: Todo[],
 }> = (props) => {
   return (
     <ul>
-        {props.items.map(todo => <li key={todo.id}>{todo.text}</li>)}
+        {props.items.map(todo => <TodoItem key={todo.id} todo={todo} />)}
     </ul>
   )
 }
